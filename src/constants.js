bdff7968f9ad54f5f535a5aa765f7823737f4de0
panchaoneu <pan.chao@northeastern.edu>
2022-08-04 00:29:53 -0700

    part1

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SAT_API_KEY = "9QQF3B-Q6S6ZW-EEG9ZL-4WPQ";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+// export const BASE_URL = "";
