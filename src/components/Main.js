8679b76eb0a1cfaa120e62d3583539e5c93d1958
panchaoneu <pan.chao@northeastern.edu>
2022-08-11 09:58:05 -0700

    from intelliji

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,56 @@
 import React, {Component} from 'react';
 import { Row, Col } from 'antd';
 import axios from 'axios';
+import {BASE_URL, NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY } from '../constants';
 import SatSetting from './SatSetting';
 import SatelliteList from './SatelliteList';
-import {BASE_URL, NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "../constants";
+import WorldMap from './WorldMap';
 
 class Main extends Component {
     constructor(){
         super();
         this.state = {
             satInfo: null,
-            settings: null,
+            satList: null,
+            setting: null,
             isLoadingList: false
-        };
+        }
+    }
+    render() {
+        const { isLoadingList, satInfo, satList, setting } = this.state;
+        return (
+            <Row className='main'>
+                <Col span={8} className="left-side">
+                    <SatSetting onShow={this.showNearbySatellite}/>
+                    <SatelliteList isLoad={isLoadingList}
+                                   satInfo={satInfo}
+                                   onShowMap={this.showMap} />
+                </Col>
+                <Col span={16} className="right-side">
+                    <WorldMap satData={satList} observerData={setting} />
+                </Col>
+            </Row>
+        );
+    }
+
+    showMap = (selected) => {
+        this.setState(preState => ({
+            ...preState,
+            satList: [...selected]
+        }))
     }
 
     showNearbySatellite = (setting) => {
         this.setState({
-            settings: setting
+            isLoadingList: true,
+            setting: setting
         })
         this.fetchSatellite(setting);
     }
 
     fetchSatellite= (setting) => {
         const {latitude, longitude, elevation, altitude} = setting;
-        const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+        const url = `${BASE_URL}/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
 
    this.setState({
        isLoadingList: true
@@ -41,24 +67,6 @@ class Main extends Component {
        .catch(error => {
            console.log('err in fetch satellite -> ', error);
        })
-}
-
-    render() {
-        const { satInfo, isLoadingList } = this.state;
-        return (
-            <Row className='main'>
-                <Col span={8} className='left-side'>
-                    <SatSetting onShow={this.showNearbySatellite}/>
-                    <SatelliteList satInfo={satInfo}
-                                   isLoad={isLoadingList}
-                    />
-                </Col>
-                <Col span={16} className="right-side">
-                    right
-                </Col>
-            </Row>
-        );
     }
 }
-
 export default Main;
